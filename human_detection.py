a1d176b11d5edcca7abe629da91ecb6cec9d53bd
YogeshRajgure <noobcoderyogesh.vraj@gmail.com>
2022-01-18 16:36:48 +0530

    Added new file image_op.py to dect on images

--- human_detection.py
+++ human_detection.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2
 import numpy as np
 from imutils.object_detection import non_max_suppression
@@ -16,17 +18,16 @@ def Detector(frame):
                                             scale=1.03)
     #we are using multiscale, as it will help to detect multiple objects in a frame
     rects = np.array([[x, y, x+w, y+h] for (x,y,w,h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.50)
     # above method helps to avoid the rect if there is an overlap of more than 65%
     c=1
-
 
     for x, y, w, h in pick:
         cv2.rectangle(frame, (x,y), (w,h),(139,34,104),2)
         cv2.rectangle(frame, (x,y-20), (w,y),(139,34,104),-1)
         cv2.putText(frame, f'P{c}',(x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255),2)
         c+=1
-
+    print("reached here")
     cv2.putText(frame,
                 f'Total Persons : {c - 1}',
                 (20,450),
--- image_op.py
+++ image_op.py
@@ -2,7 +2,7 @@ import cv2
 import imutils
 from human_detection import Detector
 
-img = cv2.imread('path_of_image')
+img = cv2.imread('abcde.jpg')
 img = imutils.resize(img, width=700)
 img = Detector(img)
 cv2.waitKey(0)
